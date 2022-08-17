53dfb4db1f72f17ba5ddbf18918d816aed923949
Dominic Tarr <dominic.tarr@gmail.com>
2022-08-17 17:54:45 +1200

    test passes ts

--- test/ts-queue.js
+++ test/ts-queue.js
@@ -6,11 +6,13 @@ var TsQueue = require('../ts-queue')
 test('simple', function (t) {
 
   var q = new TsQueue()
-  var called = false
+  var called = false, ts = null
   t.equal(q.ts, 0)
-  q.delay(10, ()=>{called = true})
+  q.delay(10, (_ts)=>{ts = _ts; called = true})
   t.equal(q.ts, 0)
+  t.equal(ts, null)
   q.drain(11)
+  t.equal(ts, 10)
   t.equal(q.ts, 10)
   t.equal(called, true)
   t.end()
@@ -32,7 +34,6 @@ test('timer', function (t) {
   q.drain(1000)
   t.equal(q.ts, 991)
   t.equal(called, 100)
-  console.log(q)
   t.end()
 
 })
